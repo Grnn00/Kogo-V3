b580b167cf232a4b834c75d7e539807ba8469bf7
Alex Garnerone <alexgarnerone2003@gmail.com>
2023-08-25 00:44:06 +0200

    almost completed GUI and ready to work with

--- src/com/metanit/MainGui.java
+++ src/com/metanit/MainGui.java
@@ -10,63 +10,113 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import javax.swing.ImageIcon;
-import java.awt.Image;
 
 
 public class MainGui extends JFrame {
-
-    JFrame frame;
-   /* ImageIcon homeIcon = new ImageIcon("dnd.jpg");
-    Image img = homeIcon.getImage();
-    Image mod = img.getScaledInstance(50,50,Image.SCALE_SMOOTH);
-    ImageIcon icon = new ImageIcon(mod);
-    JLabel pos = new JLabel(icon);
-    JLabel lab = new JLabel("sbusbbu");*/
-
-
-    private JButton AddButton = new JButton("Add");
-    private JButton DoneButton = new JButton("Done");
-    private JButton DispButton = new JButton("Display");
+    //private JLabel immagineLabel;
+    private JButton pulsante1, pulsante2, pulsante3,saveButton,loadButton;
+    private JLabel gifLabel;
 
 
     public MainGui() {
 
-        frame = new JFrame("Kogo Game");
-       frame.setBounds(300,300,400,400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //pos.setBounds(10,10,100,100);
+        this.setTitle("Interfaccia GIF");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setLayout(null);
+        
+        Container conteiner = this.getContentPane();
 
-        JInternalFrame frame1 = new JInternalFrame("Frame 1", false, false, true, true);
-        frame1.setSize(200, 150);
-        frame1.setLocation(0, 0);
-        frame1.setVisible(true);
+        JLabel immagineLabel = new JLabel();
+        ImageIcon image = new ImageIcon("dnd.jpg");
+        ImageIcon resimage = resizeImageIcon(image, 100, 100);
+        immagineLabel.setIcon(resimage);
+        immagineLabel.setBounds(10, 10,100,100);
+        conteiner.add(immagineLabel);
 
-        try {
-            BufferedImage image = ImageIO.read(new File("dnd.jpg"));
-            Image mod = image.getScaledInstance(100,100,Image.SCALE_SMOOTH);
-            JLabel label = new JLabel(new ImageIcon(mod));
-            label.setBounds(10,10,100,100);
+        JLabel saldoLabel = new JLabel("SAS");
+       // saldoLabel.setFont(new Font("Arial", Font.BOLD, 12));
+        saldoLabel.setBounds(10,120,50,10);
+        conteiner.add(saldoLabel);
 
-            frame1.add(label, BorderLayout.PAGE_START);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        JInternalFrame frame2 = new JInternalFrame("Frame 2", false, false, true, true);
-        frame2.setSize(200, 150);
-        frame2.setLocation(200, 0);
-        frame2.setVisible(true);
+        gifLabel = new JLabel("dddddvwx");
+        gifLabel.setBounds(370, 20, 400, 600);
+        conteiner.add(gifLabel);
 
-        add(frame1);
-        add(frame2);
+        JPanel pulsantiPanel = new JPanel();
+        pulsantiPanel.setBounds(0, 0, getWidth(), getHeight());
+        pulsantiPanel.setLayout(null);
 
-        //frame.add(pos);
-       // frame.add(lab);
-       // frame.pack();
-       // frame.setVisible(true);
+        pulsante1 = new JButton("Pulsante 1");
+        pulsante2 = new JButton("Pulsante 2");
+        pulsante3 = new JButton("Pulsante 3");
+        
+       // pulsante1.setPreferredSize(new Dimension(80, 20));
+        pulsante1.setBounds(10,150,80,20);
+        pulsante2.setBounds(100,150,80,20);
+        pulsante3.setBounds(190,150,80,20);  
 
+        pulsante1.addActionListener(new PulsanteListener());
+        pulsante2.addActionListener(new PulsanteListener());
+        pulsante3.addActionListener(new PulsanteListener());
 
+        pulsantiPanel.add(pulsante1);
+        pulsantiPanel.add(pulsante2);
+        pulsantiPanel.add(pulsante3);
+        conteiner.add(pulsantiPanel);
 
+        JComboBox cb=new JComboBox();
+        cb.setBounds(10, 180,100,30);       
+        cb.addItem("1");
+        cb.addItem("33");
+        cb.setSelectedIndex(0);
+        conteiner.add(cb);
 
+        JPanel savePanel = new JPanel();
+        savePanel.setBounds(0, 0, getWidth(), getHeight());
+        savePanel.setLayout(null);
+
+        saveButton= new JButton("Save");
+        loadButton = new JButton("Load Save");
+        saveButton.setBounds(10,250,100,20);
+        loadButton.setBounds(10,290,100,20);
+        savePanel.add(saveButton);
+        savePanel.add(loadButton);
+        conteiner.add(savePanel);
+        
+       
+
+        this.setVisible(true);
     }
+
+    private class PulsanteListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource().equals(pulsante1)) {
+                playGIF("src/com/metanit/gif1.gif");
+            } else if (e.getSource().equals(pulsante2)) {
+                playGIF("src/com/metanit/gif2.gif");
+            } else if (e.getSource().equals(pulsante3)) {
+                //playGIF("path_to_gif3.gif");
+                System.out.println("PALLE");
+            }
+        }
+    }
+
+    private void playGIF(String path) {
+        try {
+            ImageIcon gifIcon = new ImageIcon(path);
+            gifLabel.setIcon(gifIcon);
+            gifLabel.setVisible(true);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+        public static ImageIcon resizeImageIcon(ImageIcon originalIcon, int width, int height) {
+            Image originalImage = originalIcon.getImage();
+            Image resizedImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            return new ImageIcon(resizedImage);
+        }
+
+
 }
